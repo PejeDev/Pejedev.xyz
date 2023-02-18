f598169ab6235838d93fb1c54289edc7f112fe99
PejeDev <hi@pejedev.xyz>
2023-02-18 02:13:42 -0500

    init: nuxt and notion config

---
+++ src/pages/about.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+const { $notion } = useNuxtApp();
+const { data } = await useAsyncData("notion-index", () => $notion.getPageBlocks("49d4eb00912e47b484216fe184a5e179"));
+</script>
+
+<template>
+  <div>
+    <NotionRenderer :blockMap="data" fullPage prism katex />
+  </div>
+</template>
