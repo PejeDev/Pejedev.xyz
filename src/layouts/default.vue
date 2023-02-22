b92b137b913c8bc28b36bedcde3e429d2c498473
PejeDev <hi@pejedev.xyz>
2023-02-22 04:36:58 -0500

    feat: about me & default layout

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,6 +1,38 @@
 <template>
-  <div>
-    Some default layout shared across all pages
-    <slot />
-  </div>
+  <n-config-provider :theme="darkTheme">
+    <div class="web-content">
+      <NavBar />
+      <slot />
+      <Footer />
+    </div>
+  </n-config-provider>
 </template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import Footer from "../components/core/footer.vue";
+import NavBar from "../components/core/navbar.vue";
+import { darkTheme } from "naive-ui";
+
+export default defineComponent({
+  name: "DefaultLayout",
+  layaout: "default",
+  components: {
+    Footer,
+    NavBar,
+  },
+  data() {
+    return {
+      darkTheme,
+    };
+  },
+});
+</script>
+
+<style scoped>
+.web-content {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+}
+</style>
