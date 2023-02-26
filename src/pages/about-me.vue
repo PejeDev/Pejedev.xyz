5981a3ed2b2f75a762880d20f0631a2f7186c70e
PejeDev <hi@pejedev.xyz>
2023-02-26 00:30:27 -0500

    fix: traling slash and unnecesary consoles

--- src/pages/about-me.vue
+++ src/pages/about-me.vue
@@ -2,8 +2,6 @@
 const { $notion } = useNuxtApp();
 const { data } = await useAsyncData("notion-index", () => $notion.getPageBlocks("Juan-Camilo-Casta-eda-Mendez-49d4eb00912e47b484216fe184a5e179"));
 
-console.log(data);
-
 useSeoMeta({
   title: 'About Me',
   description: "I am a Senior Software Developer based in Bogota, Colombia. I am currently working at 9North as a Senior Frontend Developer. Also at my free time slaying some bugs and adding new features to M-35 as Main Video Game Developer.",
